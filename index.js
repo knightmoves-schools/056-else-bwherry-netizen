bdd6be5f25d0689b428486fcf6ea9e9c0009b5e1
Brendon W. <bwherry@midland.k12.ia.us>
2025-11-17 12:14:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function calculateAward (points) {
+  if (points > 99) {
+    return "First Place";
+  }
+
+  if (points > 49 && points < 100){
+    return "Second Place";
+  }
+
+  if (pints < 49){
+    return "Participation Award";
+  }  
+}
+
+
+
+
 
